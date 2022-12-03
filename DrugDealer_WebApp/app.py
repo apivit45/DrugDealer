ea6688fe2b0afc08723b453a2f5fd04a7f5d198a
Neppy-Purppy <apiwit.chn@student.mahidol.edu>
2022-12-03 20:03:56 +0700

    add mysql connector

--- DrugDealer_WebApp/app.py
+++ DrugDealer_WebApp/app.py
@@ -1,4 +1,16 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+import mysql.connector
+
+mydb = mysql.connector.connect(
+	host='localhost',
+	username='root',
+	password='',
+	port=3306
+)
 
 app = Flask(__name__)
 
